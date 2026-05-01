0a60101ac94747269ee9789e2e9754401e3cfa2b
A7med1084 <ahmed1084na@gmail.com>
2026-05-01 12:29:39 +0300

    make a scroll up button

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 "use strict";
 
+const fill = document.querySelector(".fill");
+
 window.addEventListener("scroll", function () {
   if (window.scrollY > 50) {
-    document.querySelector(".fa-arrow-up").style.opacity = 1;
+    document.querySelector(".up").style.opacity = 1;
     document.querySelector(".header").classList.add("shadow");
 } else {
-    document.querySelector(".fa-arrow-up").style.opacity = 0;
+    document.querySelector(".up").style.opacity = 0;
     document.querySelector(".header").classList.remove("shadow");
   }
+  let value = window.scrollY;
+  let pageHeight = document.documentElement.scrollHeight;
+  fill.style.backgroundImage = `conic-gradient( #186baf 0deg, #186baf ${value / (pageHeight - 810) * 360}deg, transparent 0`
+  console.log(pageHeight);
 });
+
